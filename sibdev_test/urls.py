45e11c4fabffcb6d642e607c9ca9cbd14d736de4
Lev Sentsov <l.sentsov@napoleonit.com>
2023-02-26 23:59:32 +0600

    fix depend-s home page view

--- sibdev_test/urls.py
+++ sibdev_test/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from deals.views.home_page_view import home_page_view
 
+
 urlpatterns = [
     path('', home_page_view, name='home'),
     path('deals/', include('deals.urls')),
